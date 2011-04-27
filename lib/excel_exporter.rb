1ecc61ab1408a6c1ea2ce18ea037ed64f4232fd7
gshilin <gshilin@gmail.com>
2011-04-27 11:32:58 +0300

    first commit

---
+++ lib/excel_exporter.rb
@@ -0,0 +1,113 @@
+class Array
+  ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:excel]='%Y-%m-%dT%H:%M:%SZ'
+
+  # Options:
+  #
+  # :only => only those columns will be exported
+  # :except => all columns except these ones will be exported
+  # Note: only one of the above options can be used at the same time
+  #
+  # :methods => list of columns to be exported that don't correspond to DB columns
+  # :headers => Humanized name of column will be used as header by default;
+  #             You can specify here a list of headers that must be used instead of default ones
+  # :title   => name of workbook
+  #
+  # Example:
+  #
+  #    list = Purchase.purchases_by_product(id).all
+  #    fields = [:id, :username]
+  #    headers = {:id => 'שובר', :username => 'שם'}
+  #
+  #    list.to_excel(
+  #        :title => "#{Time.now.strftime("%d-%m-%Y")}_#{id}",
+  #        :only => fields,
+  #        :headers => headers,
+  #        :types => {:product_id => 'Number', :user_id => 'Number', :price => 'Number'}
+  #    )
+
+  def to_excel(options = {})
+    return '' if self.empty?
+
+    klass = self.first.class
+    attributes = self.first.attributes.keys.sort.map(&:to_sym)
+
+    if options[:only]
+      columns = Array(options[:only]) & attributes
+    else
+      columns = attributes - Array(options[:except])
+    end
+
+    columns += Array(options[:methods])
+
+    return '' if columns.empty?
+
+    time_now = Time.now
+
+    header_names = options[:headers] || {}
+
+    headers = columns.map { |column|
+      name = (header_names && header_names[column]) ? header_names[column] : klass.human_attribute_name(column)
+      "<Cell ss:StyleID='header'><Data ss:Type='String'>#{name}</Data></Cell>"
+    }
+
+    col_defs = columns.map { |column|
+      '<Column ss:Width="141"/>'
+    }
+
+    types = options[:types] || {}
+    
+    body = self.map { |item|
+      cols = columns.map { |colname|
+        value = item.send(colname)
+        type = colname == :id ? 'Number' : (types[colname] || 'String')
+        "<Cell><Data ss:Type='#{type}'>#{value}</Data></Cell>"
+      }
+      "<Row>" + cols.join("\n") +"</Row>"
+    }
+
+    title = options[:title] || "#{time_now.strftime("%d-%m-%Y")}_#{options[:id]}"
+    
+    output = <<-OUTPUT
+<?xml version="1.0"?>
+<?mso-application progid="Excel.Sheet"?>
+<Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet"
+ xmlns:o="urn:schemas-microsoft-com:office:office"
+ xmlns:x="urn:schemas-microsoft-com:office:excel"
+ xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet"
+ xmlns:html="http://www.w3.org/TR/REC-html40">
+ <DocumentProperties xmlns="urn:schemas-microsoft-com:office:office">
+  <Author>baligam</Author>
+  <LastAuthor>baligam</LastAuthor>
+  <Company>baligam</Company>
+  <Created>#{time_now.to_s(:excel)}</Created>
+  <LastSaved>#{time_now.to_s(:excel)}</LastSaved>
+ </DocumentProperties>
+ <Styles>
+  <Style ss:ID="Default" ss:Name="Normal">
+   <Alignment ss:Vertical="Bottom"/>
+   <Borders/>
+   <Font ss:FontName="Calibri" x:Family="Swiss" ss:Size="11" ss:Color="#000000"/>
+   <Interior/>
+   <NumberFormat/>
+   <Protection/>
+  </Style>
+  <Style ss:ID="header">
+    <Alignment ss:Horizontal="Center"/>
+    <Font ss:Size="8" ss:Bold="1"/>
+    <Interior ss:Color="#d0d0d0" ss:Pattern="Solid"/>
+  </Style>
+ </Styles>
+ <Worksheet ss:Name="#{title}">
+  <Table ss:ExpandedColumnCount="#{columns.count}" ss:ExpandedRowCount="#{self.count + 1}" x:FullColumns="1" x:FullRows="1" ss:DefaultRowHeight="15">
+   #{ col_defs.join("\n") }
+   <Row>#{ headers.join("\n") }</Row>
+    #{ body.join("\n") }
+  </Table>
+ </Worksheet>
+</Workbook>
+    OUTPUT
+
+    output
+  end
+end
+	
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '47243cd6ef55ab3e45e9ecfbc8b404ce7b7c2b36d5f0aa0e5f229a05efec7dfbcb09c80c15005b3de6fd7dd5372686ed18ed08cceac1d0313702db6b0cc28552'
