58d6bdbc2f53158e92547b3b65b885335f38111c
Rami Grossman <ramigg@gmail.com>
2012-10-24 13:14:44 +0200

    added support for PG::Result

--- lib/excel_exporter.rb
+++ lib/excel_exporter.rb
@@ -1,3 +1,93 @@
+module PG
+  class Result
+    def to_excel(options = {})
+
+      return '' if self.count.zero?
+
+      attributes = self.first.keys.map(&:to_sym)
+
+      if options[:only]
+        columns = Array(options[:only]) & attributes
+      else
+        columns = attributes - Array(options[:except])
+      end
+
+      return '' if columns.empty?
+
+      time_now = Time.now
+
+      header_names = options[:headers] || {}
+
+      headers = columns.map do |column|
+        name = (header_names && header_names[column]) ? header_names[column] : column.to_s.humanize
+        "<Cell ss:StyleID='header'><Data ss:Type='String'>#{name}</Data></Cell>"
+      end
+
+      col_defs = columns.map { |column|
+        '<Column ss:Width="141"/>'
+      }
+
+      types = options[:types] || {}
+      coder = HTMLEntities.new
+
+      body = self.map do |item|
+        cols = columns.map { |colname|
+          value = item[colname.to_s]
+          type = types[colname] || 'String'
+          "<Cell><Data ss:Type='#{type}'>#{coder.encode(value)}</Data></Cell>"
+        }
+        "<Row>" + cols.join("\n") +"</Row>"
+      end
+
+      title = options[:title] || "#{time_now.strftime("%d-%m-%Y")}_#{options[:id]}"
+
+      output = <<-OUTPUT
+  <?xml version="1.0"?>
+  <?mso-application progid="Excel.Sheet"?>
+  <Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet"
+   xmlns:o="urn:schemas-microsoft-com:office:office"
+   xmlns:x="urn:schemas-microsoft-com:office:excel"
+   xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet"
+   xmlns:html="http://www.w3.org/TR/REC-html40">
+   <DocumentProperties xmlns="urn:schemas-microsoft-com:office:office">
+    <Author>#{options[:author] || 'Unknown'}</Author>
+    <LastAuthor>#{options[:author] || 'Unknown'}</LastAuthor>
+    <Company>#{options[:company] || 'Unknown'}</Company>
+    <Created>#{time_now.strftime('%Y-%m-%dT%H:%M:%SZ')}</Created>
+    <LastSaved>#{time_now.strftime('%Y-%m-%dT%H:%M:%SZ')}</LastSaved>
+   </DocumentProperties>
+   <Styles>
+    <Style ss:ID="Default" ss:Name="Normal">
+     <Alignment ss:Vertical="Bottom"/>
+     <Borders/>
+     <Font ss:FontName="Calibri" x:Family="Swiss" ss:Size="11" ss:Color="#000000"/>
+     <Interior/>
+     <NumberFormat/>
+     <Protection/>
+    </Style>
+    <Style ss:ID="header">
+      <Alignment ss:Horizontal="Center"/>
+      <Font ss:Size="8" ss:Bold="1"/>
+      <Interior ss:Color="#d0d0d0" ss:Pattern="Solid"/>
+    </Style>
+   </Styles>
+   <Worksheet ss:Name="#{title}">
+    <Table ss:ExpandedColumnCount="#{columns.length}" ss:ExpandedRowCount="#{self.count + 1}" x:FullColumns="1" x:FullRows="1" ss:DefaultRowHeight="15">
+     #{ col_defs.join("\n") }
+     <Row>#{ headers.join("\n") }</Row>
+      #{ body.join("\n") }
+    </Table>
+   </Worksheet>
+  </Workbook>
+      OUTPUT
+
+      output
+    end
+
+  end
+end
+
+
 class Array
 
   # Options:
@@ -46,10 +136,10 @@ class Array
 
     header_names = options[:headers] || {}
 
-    headers = columns.map { |column|
+    headers = columns.map do |column|
       name = (header_names && header_names[column]) ? header_names[column] : klass.human_attribute_name(column)
       "<Cell ss:StyleID='header'><Data ss:Type='String'>#{name}</Data></Cell>"
-    }
+    end
 
     col_defs = columns.map { |column|
       '<Column ss:Width="141"/>'
@@ -57,18 +147,18 @@ class Array
 
     types = options[:types] || {}
     coder = HTMLEntities.new
-    
-    body = self.map { |item|
+
+    body = self.map do |item|
       cols = columns.map { |colname|
         value = item.send(colname)
         type = colname == :id ? 'Number' : (types[colname] || 'String')
         "<Cell><Data ss:Type='#{type}'>#{coder.encode(value)}</Data></Cell>"
       }
       "<Row>" + cols.join("\n") +"</Row>"
-    }
+    end
 
     title = options[:title] || "#{time_now.strftime("%d-%m-%Y")}_#{options[:id]}"
-    
+
     output = <<-OUTPUT
 <?xml version="1.0"?>
 <?mso-application progid="Excel.Sheet"?>
